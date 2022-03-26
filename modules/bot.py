a57bb202a47c1f24cf3c773ff7f77848c6407ff6
OREdwardsJr <97556216+OREdwardsJr@users.noreply.github.com>
2022-03-26 00:40:58 -0700

    Initial functionality added. Bot capable of watching and responding to messages within discord server

---
+++ modules/bot.py
@@ -0,0 +1,40 @@
+import discord
+from dotenv import load_dotenv
+import random
+load_dotenv('---.env')
+import os
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == "bots-area":
+        if user_message.lower() == 'hello':
+            await message.channel.send(f'Hello {username}')
+            return
+        elif user_message.lower() == 'bye':
+            await message.channel.send(f'See you later {username}!')
+            return
+        elif user_message.lower() == '!random':
+            response = f'This is your random number: {random.randrange(10000000)}'
+            await message.channel.send(response)
+            return
+    if user_message.lower() == '!anywhere':
+        await message.channel.send('This can be used anywhere')
+        return
+
+client.run(TOKEN)
